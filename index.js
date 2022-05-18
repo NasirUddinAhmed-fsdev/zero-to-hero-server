3bae96fcdab6ab792ff93502bb5a21ac14054426
Nash0123 <2191091086@uttarauniversity.edu.bd>
2022-05-18 20:49:35 +0600

    server updated

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Server is ready");
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//! Warning: Do not use for production
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y583n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("zero-hero").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const services = await serviceCollection.find({}).toArray();
+      // console.log(services);
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
